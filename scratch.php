042f051794798aefe04784c2e411590ad4478a35
rdaitan <r.daitan@klab.com>
2015-08-06 14:56:33 +0800

    Added variable identifiers example.

--- scratch.php
+++ scratch.php
@@ -2,7 +2,7 @@
 echo "hello";   // a statement
 echo " world";  // a statement must end with a semicolon
 
-if(false) {
+if (false) {
     echo "!";   // the semicolon before the closing brace is required.
 }               //no need for a semicolon after a curly brace
 
@@ -29,4 +29,23 @@ print(
 multiline
 comment
 */
+
+?>
+
+<?php
+echo "Hello, world";
+echo 1 + 1;
+echo true;
+?>
+
+<?php
+// valid
+$identifier123;
+$Identifier123;
+$_identifier123;
+
+// invalid
+// $123identifier;
+
+
 

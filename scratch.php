2482f72c51bbe06e464d2da031071da066fd44d2
rdaitan <r.daitan@klab.com>
2015-08-06 13:50:38 +0800

    Created scratch.php

---
+++ scratch.php
@@ -0,0 +1,10 @@
+<?php
+echo "hello";   // a statement
+echo " world";  // a statement must end with a semicolon
+
+if(false) {
+    echo "!";   // the semicolon before the closing brace is required.
+}               //no need for a semicolon after a curly brace
+
+echo ":)"       // no need for a semicolon before the closing php tag
+?>

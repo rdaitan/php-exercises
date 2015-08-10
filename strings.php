3c969928f38b51996a716108fdcbcb22713b3995
rdaitan <r.daitan@klab.com>
2015-08-10 16:00:34 +0800

    Added string offset example.

--- strings.php
+++ strings.php
@@ -22,6 +22,13 @@ echo '</p>';
 $str_format = "%'&+8.2d";
 printf($str_format, 1984);
 
+
+// string offset
+$alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+for($i = 1; $i <= 26; $i = $i << 1)
+    printf("%s ", $alphabet{$i - 1});
+    
+
 ?>
 </body>
 </html>

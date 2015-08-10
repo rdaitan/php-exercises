fcdc40fab08925a3781212512c9596df15be70b5
rdaitan <r.daitan@klab.com>
2015-08-10 08:04:35 +0800

    Added direct echo example.

---
+++ direct-echo.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Direct Echo</title>
+    </head>
+    <body>
+        <p>Is this the <?= "Krusty Krab" ?>?</p><!--SGML-STYLE WORKS BY DEFAULT-->
+        <p>No, this is <%= "Patrick" %>.</p><!--ASP-STYLE DOES NOT WORK BY DEFAULT-->
+    </body>
+</html>

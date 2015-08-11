2d13c88315dfe402015a6f41847d47f44c98fa57
rdaitan <r.daitan@klab.com>
2015-08-11 14:38:02 +0800

    Commented out array creation example.

--- arrays.php
+++ arrays.php
@@ -4,5 +4,6 @@ print("1 {$arr[0]}");
 print("2 {$arr["snake"]}");
 
 // works as of php 5.4
-$arr2 = ['y' => 'yes', 'n' => 'no', 'm' => 'maybe'];
-var_dump($arr2);
+//$arr2 = ['y' => 'yes', 'n' => 'no', 'm' => 'maybe'];
+//var_dump($arr2);
+

e27d152e1b0c2062fae9c1e7814f129985ce9c5a
rdaitan <r.daitan@klab.com>
2015-08-11 11:26:25 +0800

    Added pipe examples.

--- regex.php
+++ regex.php
@@ -4,3 +4,10 @@ var_dump(preg_match('/\$5/', '5 only.'));
 // remember that php would expand $variable when using
 // double quotes.
 
+print('<br/>');
+
+// pipe
+var_dump(preg_match('/cat|dog/', 'catman'));
+var_dump(preg_match('/cat|dog/', 'dogman'));
+var_dump(preg_match('/cat|dog/', 'catog'));
+var_dump(preg_match('/cat|dog/', 'dog'));

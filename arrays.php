c7c3c53ebf02fbe2e109f6fb8c37cae150dbe1f2
rdaitan <r.daitan@klab.com>
2015-08-11 14:36:33 +0800

    Added array creation example.

--- arrays.php
+++ arrays.php
@@ -2,3 +2,7 @@
 $arr = array('Dog', 'snake' => 'Snake', 'Cat');
 print("1 {$arr[0]}");
 print("2 {$arr["snake"]}");
+
+// works as of php 5.4
+$arr2 = ['y' => 'yes', 'n' => 'no', 'm' => 'maybe'];
+var_dump($arr2);

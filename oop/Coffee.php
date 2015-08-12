b48f56d1df2885f472a6c72f6e02ee9d8931aec1
rdaitan <r.daitan@klab.com>
2015-08-12 13:09:02 +0800

    Added overloading methods to Coffee class.

--- oop/Coffee.php
+++ oop/Coffee.php
@@ -20,4 +20,17 @@ class Coffee {
     static function printp(Coffee $coffee) {
         print("<p>{$coffee->getFullName()} | {$coffee->net_wt}</p>");
     }
+    
+    public function __set($name, $value) {
+        $this->{$name} = $value;
+    }
+    
+    public function __get($name) {
+        return $this->{$name};
+    }
+    
+    // for method overloading
+    public function __call($name, $args) {
+        return call_user_func($this->{$name}, $args);
+    }
 }

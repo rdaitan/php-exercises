894898b55ca11b5002f1acc85867126a9ce2b269
rdaitan <r.daitan@klab.com>
2015-08-12 11:29:53 +0800

    Added functions to Coffee class.

--- oop/Coffee.php
+++ oop/Coffee.php
@@ -2,5 +2,22 @@
 class Coffee {
     public $name;
     public $brand;
-    public $net_wt;    
+    public $net_wt;
+    
+    public function getFullName() {
+        return $this->upperCaseBrand() . " {$this->name}";
+    }
+    
+    public function getInfo() {
+        return "Brand: {$this->brand}\nName: {$this->name}\nNet Wt. {$this->net_wt}";
+    }
+        
+    
+    private function upperCaseBrand() {
+        return strtoupper($this->brand);
+    }
+    
+    static function printp(Coffee $coffee) {
+        print("<p>{$coffee->getFullName()} | {$coffee->net_wt}</p>");
+    }
 }

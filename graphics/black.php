5a0c4ae2ad965035d8e9f8faba64b2a79208104a
rdaitan <r.daitan@klab.com>
2015-08-17 10:53:40 +0800

    Moved header call.

--- graphics/black.php
+++ graphics/black.php
@@ -2,11 +2,13 @@
 # a black square on a white background
 # make sure you have gd extensions installed.
 # sudo apt-get install php5-gd
-header("Content-Type: image/png");
 
 $image = imagecreate(200, 200);
 $white = imagecolorallocate($image, 0xFF, 0xFF, 0xFF);
 $black = imagecolorallocate($image, 0x00, 0x00, 0x00);
 imagefilledrectangle($image, 50, 50, 150, 150, $black);
 
+# setting the header at the top would treat any error as part of the image.
+# this results in a broken image being displayed.
+header("Content-Type: image/png");
 imagepng($image); // output a png image

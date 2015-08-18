d773c05006897d313be74b1d32556bf371823b6a
rdaitan <r.daitan@klab.com>
2015-08-18 12:51:53 +0800

    Created form_template.php
    form_template.php processes the templates the based on whether the user submitted a name or not.

---
+++ templating/form_template.php
@@ -0,0 +1,14 @@
+<?php
+$bindings = array();
+$template = '';
+
+if (array_key_exists('NAME', $_GET)) {
+  $template = 'thankyou.template';
+  $bindings['NAME'] = $_GET['NAME'];
+}
+else {
+  $template = 'user.template';
+}
+
+$bindings['DESTINATION'] = $_SERVER['PHP_SELF'];
+echo fillTemplate($template, $bindings);
